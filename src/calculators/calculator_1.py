cbb2299f8cc0a80de9a7e5051e23f3d89383f27d
Maria Andreza C Santos <mandreza008@gmail.com>
2025-01-19 19:57:09 -0300

    feat: finished calculator

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -4,8 +4,13 @@ from flask import request as FlaskRequest
 class Calculator1:
     '''
     - Um numero é divido em 3 partes iguais. CHECK
-    - A primeira parte é dividida por 4 e seu resultado somado a 7.
-    - Após isso, o resultado é elevado ao quadrado e multiplicado por um valor de 0.257.
+    - A primeira parte é dividida por 4 e seu resultado somado a 7. CHECK
+    - Após isso, o resultado é elevado ao quadrado e multiplicado por um valor de 0.257. CHECK
+
+    - A segunda parte é elevada a potência de 2.121, dividida por 5 e somado a 1.
+    - A terceira parte se mantem no mesmo valor.
+
+    - Por fim, é somado esses 3 valores e entregue o resultado.
     '''
 
     def calculate(self, request: FlaskRequest):
@@ -14,6 +19,11 @@ class Calculator1:
         splited_number = input_data / 3
 
         first_process_result = self.__first_process(splited_number)
+        second_process_result = self.__second_process(splited_number)
+        calc_result = first_process_result + second_process_result + splited_number
+        response = self.__format_response(calc_result)
+
+        return response
 
     def __validate_body(self, body: Dict) -> float:
         if "number" not in body:
@@ -26,3 +36,16 @@ class Calculator1:
         first_part = (first_number / 4) + 7
         second_part = (first_part ** 2) * 0.257
         return second_part
+    
+    def __second_process(self, second_number: float) -> float:
+        first_part = (second_number ** 2.121)
+        second_part = (first_part / 5) + 1
+        return(second_part)
+    
+    def __format_response(self, calc_result: float) -> Dict:
+        return {
+            "data": {
+                "Calculator": 1,
+                "result": round(calc_result, 2)
+            }
+        }
