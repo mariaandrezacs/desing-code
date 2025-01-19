2e204fe8bbb1d79109fe0884d78496c565e635cd
Maria Andreza C Santos <mandreza008@gmail.com>
2025-01-19 19:56:18 -0300

    feat: configuration routes

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -7,5 +7,5 @@ calc_rout_bp = Blueprint("calc_routes", __name__)
 def calculator_1():
     # print(request.json)     # caputura o body da requisição
     calc = Calculator1()
-    calc.calculate(request)
-    return jsonify({"sucess": True})
+    response = calc.calculate(request)
+    return jsonify(response)
