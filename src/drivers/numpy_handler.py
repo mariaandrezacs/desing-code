ed28834ca9c65cba5b31a1cc14bf0b1f5ca84824
mariaandrezacs <mandreza008@gmail.com>
2025-01-22 12:45:02 -0300

    buid: added library numpy and facade

---
+++ src/drivers/numpy_handler.py
@@ -0,0 +1,13 @@
+import numpy
+from typing import List
+
+
+class NumpyHandler:
+    def __init__(self) -> None:
+        self.__np = numpy
+
+    def standard_derivation(self, numbers=List[float]) -> float:
+        return self.__np.std(numbers)
+
+
+# trabalhando com biblioteca externas usando fachada.
