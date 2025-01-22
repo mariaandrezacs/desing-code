b6ea7a13c2d5958664991abe9eff5ace3c66bab4
mariaandrezacs <mandreza008@gmail.com>
2025-01-22 11:08:24 -0300

    test: test errors application

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,4 +1,5 @@
 from typing import Dict
+from pytest import raises
 from .calculator_1 import Calculator1
 
 
@@ -13,8 +14,8 @@ def test_calculate():
     calculator_1 = Calculator1()
 
     response = calculator_1.calculate(mock_request)
-    print()
-    print(response)
+    # print()
+    # print(response)
 
     # 1 - formato da resposta / tipagem
     assert "data" in response
@@ -24,3 +25,14 @@ def test_calculate():
     # 2 - Assertividade da reposta
     assert response["data"]["result"] == 14.25
     assert response["data"]["Calculator"] == 1
+
+
+# testanto erros
+def test_calculate_whit_body_error():
+    mock_request = MockRequest(body={"something": 1})
+    calculator_1 = Calculator1()
+
+    with raises(Exception) as excinfo:
+        calculator_1.calculate(mock_request)
+
+    assert str(excinfo.value) == "body mal formatado!"
