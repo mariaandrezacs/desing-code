0d94cc895b26fa7777683e4edb621fcac161e21d
Maria Andreza C Santos <mandreza008@gmail.com>
2025-01-26 00:05:51 -0300

    feat: configuring routes

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,20 +1,18 @@
 from flask import Blueprint, jsonify, request
-from src.calculators.calculator_1 import Calculator1
-from src.calculators.calculator_2 import Calculator2
-from src.drivers.numpy_handler import NumpyHandler
+from ..factories.calculator1_factory import calculator1_factory
+from ..factories.calculator2_factory import calculator2_factory
 
 calc_rout_bp = Blueprint("calc_routes", __name__)
 
 @calc_rout_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
     # print(request.json)     # caputura o body da requisição
-    calc = Calculator1()
+    calc = calculator1_factory()
     response = calc.calculate(request)
     return jsonify(response)
 
 @calc_rout_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
-    numpy_handler = NumpyHandler()
-    calc = Calculator2(numpy_handler)
+    calc = calculator2_factory()
     response = calc.calculate(request)
     return jsonify(response)
