020f4f7728da70c935709c270f27f3035122c021
Maria Andreza C Santos <mandreza008@gmail.com>
2025-01-25 23:39:58 -0300

    feat: calculator 2 improvement in the code

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,5 +1,6 @@
 from typing import Dict
 from .calculator_2 import Calculator2
+from ..drivers.numpy_handler import NumpyHandler
 
 
 class MockRequest:
@@ -11,7 +12,8 @@ class MockRequest:
 def test_calculate():
     mock_request = MockRequest({"numbers": [2.12, 4.62, 1.32]})
 
-    calculate_2 = Calculator2()
+    driver = NumpyHandler()
+    calculate_2 = Calculator2(driver)
     formated_response = calculate_2.calculate(mock_request)
     print()
     print(formated_response)
