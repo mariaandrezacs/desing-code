82f46ff3f83b018e0944cbae775a0eb12bb5c299
Maria Andreza C Santos <mandreza008@gmail.com>
2025-01-25 23:54:55 -0300

    feat: added route calculator 2

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,5 +1,7 @@
 from flask import Blueprint, jsonify, request
 from src.calculators.calculator_1 import Calculator1
+from src.calculators.calculator_2 import Calculator2
+from src.drivers.numpy_handler import NumpyHandler
 
 calc_rout_bp = Blueprint("calc_routes", __name__)
 
@@ -7,5 +9,12 @@ calc_rout_bp = Blueprint("calc_routes", __name__)
 def calculator_1():
     # print(request.json)     # caputura o body da requisição
     calc = Calculator1()
+    response = calc.calculate(request)
+    return jsonify(response)
+
+@calc_rout_bp.route("/calculator/2", methods=["POST"])
+def calculator_2():
+    numpy_handler = NumpyHandler()
+    calc = Calculator2(numpy_handler)
     response = calc.calculate(request)
     return jsonify(response)
