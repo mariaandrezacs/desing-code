ac0afb60e6cbf5218c501337d8202ed6c1881e88
mariaandrezacs <mandreza008@gmail.com>
2025-01-27 12:49:22 -0300

    feat: settings routes

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -2,24 +2,40 @@ from flask import Blueprint, jsonify, request
 from ..factories.calculator1_factory import calculator1_factory
 from ..factories.calculator2_factory import calculator2_factory
 from ..factories.calculator3_factory import calculator3_factory
+from ...errors.error_controller import handle_errors
 
 calc_rout_bp = Blueprint("calc_routes", __name__)
 
+
 @calc_rout_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    # print(request.json)     # caputura o body da requisição
-    calc = calculator1_factory()
-    response = calc.calculate(request)
-    return jsonify(response)
+    try:
+        calc = calculator1_factory()
+        response = calc.calculate(request)
+
+        return jsonify(response)
+    except Exception as exception:
+        error_response = handle_errors(exception)
+        return jsonify(error_response["body"]), error_response["status_code"]
+
 
 @calc_rout_bp.route("/calculator/2", methods=["POST"])
 def calculator_2():
-    calc = calculator2_factory()
-    response = calc.calculate(request)
-    return jsonify(response)
+    try:
+        calc = calculator2_factory()
+        response = calc.calculate(request)
+        return jsonify(response)
+    except Exception as exception:
+        error_response = handle_errors(exception)
+        return jsonify(error_response["body"]), error_response["status_code"]
+
 
 @calc_rout_bp.route("/calculator/3", methods=["POST"])
 def calculator_3():
-    calc = calculator3_factory()
-    response = calc.calculate(request)
-    return jsonify(response)
+    try:
+        calc = calculator3_factory()
+        response = calc.calculate(request)
+        return jsonify(response)
+    except Exception as exception:
+        error_response = handle_errors(exception)
+        return jsonify(error_response["body"]), error_response["status_code"]
