2da2c406951dc86406b33403a8f8d82a62f8b3b5
mariaandrezacs <mandreza008@gmail.com>
2025-01-27 12:48:50 -0300

    feat: added error handling

---
+++ src/errors/error_controller.py
@@ -0,0 +1,15 @@
+from typing import Dict
+from .http_unprocessable_entity import HttpUnprocessableEntity
+from .http_bad_request import HttpBadRequestError
+
+
+def handle_errors(error: Exception) -> Dict:
+    if isinstance(error, (HttpUnprocessableEntity, HttpBadRequestError)):
+        return {
+            "status_code": error.status_code,
+            "body": {"errors": [{"title": error.name, "detail": error.message}]},
+        }
+    return {
+        "status_code": 500,
+        "body": {"errors": [{"title": "Server Error", "detail": str(error)}]},
+    }
